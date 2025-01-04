78b1994ef96d1ce9d3035d01ad7a06f53eca5dcc
razboy <mishaneiman@gmail.com>
2025-01-03 22:26:11 -0500

    Added mason

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,33 +1,68 @@
-local cmp = require('cmp')
+-- lsp.lua
 
--- Autocompletion setup
+-- ============================
+-- === Import Required Modules ===
+-- ============================
+
+local cmp = require('cmp')
+local lspconfig = require('lspconfig')
+local mason = require('mason')
+local mason_lspconfig = require('mason-lspconfig')
+
+-- ============================
+-- === Diagnostic Signs Setup ===
+-- ============================
+
+-- Define custom diagnostic signs with icons
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+-- Register each diagnostic sign
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+-- ============================
+-- === Autocompletion Setup ===
+-- ============================
+
 cmp.setup({
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      require('luasnip').lsp_expand(args.body) -- Integrate LuaSnip for snippets
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm the completion
-    ['<C-Space>'] = cmp.mapping.complete(),            -- Trigger completion
-    ['<Tab>'] = cmp.mapping.select_next_item(),        -- Navigate forward in completion list
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),      -- Navigate backward in completion list
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm completion with Enter
+    ['<C-Space>'] = cmp.mapping.complete(),            -- Trigger completion with Ctrl+Space
+    ['<Tab>'] = cmp.mapping.select_next_item(),        -- Navigate to next item with Tab
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),      -- Navigate to previous item with Shift+Tab
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' }, -- LSP source
     { name = 'luasnip' },  -- Snippets source
   }, {
-    { name = 'buffer' },   -- Text from current buffer
-    { name = 'path' },     -- Filesystem paths
+    { name = 'buffer' },   -- Buffer source
+    { name = 'path' },     -- Path source
   }),
 })
 
--- LSP capabilities for completion
+-- ============================
+-- === LSP Capabilities ===
+-- ============================
+
+-- Enhance LSP capabilities for autocompletion
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
--- LSP on_attach function for keybindings and features
+-- ============================
+-- === LSP on_attach Function ===
+-- ============================
+
+-- Define the on_attach function to set keybindings and other LSP features
 local on_attach = function(client, bufnr)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+  -- Define keybindings for LSP functions
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)          -- Go to definition
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)          -- Show references
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)      -- Go to implementation
@@ -37,11 +72,100 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<leader>f', function()
     vim.lsp.buf.format({ async = true })
   end, bufopts)                                                       -- Format buffer
+
+  -- Enable document highlighting if supported
+  if client.server_capabilities.documentHighlightProvider then
+    vim.api.nvim_exec([[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]], false)
+  end
 end
 
--- Zig LSP setup with enhanced capabilities and keybindings
-require('lspconfig').zls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+-- ============================
+-- === Mason & Mason-LSPconfig ===
+-- ============================
+
+-- Setup Mason
+mason.setup({
+  ui = {
+    border = "rounded",
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
 })
 
+-- Setup Mason-LSPconfig
+mason_lspconfig.setup({
+  ensure_installed = { 'lua_ls', 'zls', 'pyright', 'clangd' }, -- List of LSP servers to install
+  automatic_installation = true, -- Automatically install missing servers
+})
+
+-- Setup handlers for installed LSP servers
+mason_lspconfig.setup_handlers({
+  -- Default handler for all LSP servers
+  function(server_name)
+    lspconfig[server_name].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+  end,
+})
+
+-- ============================
+-- === Diagnostic Configuration ===
+-- ============================
+
+-- Configure diagnostic display
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●', -- Could be '●', '▎', '■', etc.
+    spacing = 4,
+    severity_sort = true,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false, -- Don't show diagnostics while typing
+  severity_sort = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+})
+
+-- ============================
+-- === LSP Handlers ===
+-- ============================
+
+-- Enhance floating windows for hover and signature help with rounded borders
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {
+    border = "rounded",
+    focusable = false,
+    style = "minimal",
+    width = 60,
+    height = 15,
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {
+    border = "rounded",
+    focusable = false,
+    style = "minimal",
+    width = 60,
+    height = 15,
+  }
+)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,16 @@
+-- plugins.lua
+
 return require('packer').startup(function(use)
+  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
   -- ===============
   -- === THEMES ===
-  -- ===============:
+  -- ===============
   use {
     'gruvbox-community/gruvbox',
     config = function()
-      -- We actively set gruvbox as our theme
+      -- Set gruvbox as the colorscheme
       vim.cmd([[colorscheme gruvbox]])
     end
   }
@@ -28,7 +31,7 @@ return require('packer').startup(function(use)
         persist_size = true,        -- Preserve size across toggles
       })
 
-      -- Example keybinding to toggle terminal
+      -- Keybinding to toggle terminal
       vim.keymap.set('n', '<leader>tt', ':ToggleTerm<CR>', { noremap = true, silent = true })
     end,
   }
@@ -46,20 +49,37 @@ return require('packer').startup(function(use)
           side = "left",
           preserve_window_proportions = true,
         },
+        diagnostics = {
+          enable = true,
+          show_on_dirs = false,
+          icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+          },
+        },
       })
 
+      -- Keybindings to handle tree toggle and focus
+      vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>n', ':NvimTreeFocus<CR>',  { noremap = true, silent = true })
     end,
   }
+
   -- ====================
   -- === LSP & CMP    ===
   -- ====================
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'L3MON4D3/LuaSnip'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'saadparwaiz1/cmp_luasnip'
+  use 'neovim/nvim-lspconfig'               -- LSP configurations
+  use 'williamboman/mason.nvim'             -- Mason for managing LSP servers
+  use 'williamboman/mason-lspconfig.nvim'   -- Mason bridge to lspconfig
+  use 'hrsh7th/nvim-cmp'                    -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp'                -- LSP source for nvim-cmp
+  use 'L3MON4D3/LuaSnip'                    -- Snippets plugin
+  use 'hrsh7th/cmp-buffer'                  -- Buffer completions
+  use 'hrsh7th/cmp-path'                    -- Path completions
+  use 'saadparwaiz1/cmp_luasnip'            -- Snippets source for nvim-cmp
+  -- Removed 'onsails/lspkind-nvim' to exclude LSP kind icons
 
   -- ============================
   -- === TREESITTER SYNTAX    ===
@@ -87,7 +107,26 @@ return require('packer').startup(function(use)
       require('lualine').setup({
         options = {
           theme = 'auto',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
         },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        extensions = { 'nvim-tree' },
       })
     end,
   }
@@ -99,12 +138,36 @@ return require('packer').startup(function(use)
   use {
     'nvim-telescope/telescope.nvim',
     config = function()
-      require('telescope').setup({})
+      require('telescope').setup({
+        defaults = {
+          prompt_prefix = "  ",
+          selection_caret = "  ",
+          sorting_strategy = "ascending",
+          layout_strategy = "horizontal",
+          layout_config = {
+            width = 0.9,
+            height = 0.9,
+            preview_cutoff = 120,
+            prompt_position = "top",
+          },
+          mappings = {
+            i = {
+              ["<C-u>"] = false,
+              ["<C-d>"] = false,
+            },
+          },
+        },
+        pickers = {
+          -- Your picker configurations can go here
+        },
+      })
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { noremap=true, silent=true })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep,  { noremap=true, silent=true })
       vim.keymap.set('n', '<leader>fb', builtin.buffers,    { noremap=true, silent=true })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags,  { noremap=true, silent=true })
+      -- Add global live_grep keybinding
+      vim.keymap.set('n', '<leader>S', builtin.live_grep, { noremap = true, silent = true, desc = "Live Grep from anywhere" })
     end,
   }
 
@@ -135,5 +198,55 @@ return require('packer').startup(function(use)
       require('alpha').setup(require('alpha.themes.dashboard').config)
     end,
   }
-end)
 
+  -- ====================
+  -- === Mason.nvim    ===
+  -- ====================
+  use {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup({
+        ui = {
+          border = "rounded",
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end
+  }
+
+  use {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'lua_ls', 'zls', 'pyright', 'clangd' }, -- List of LSP servers to install
+        automatic_installation = true, -- Automatically install missing servers
+      })
+    end
+  }
+
+  -- ====================
+  -- === DIAGNOSTICS LIST ===
+  -- ====================
+  use {
+    "folke/trouble.nvim",
+    requires = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup({
+        icons = true,
+        signs = { error = "", warning = "", hint = "", information = "" },
+        use_diagnostic_signs = true,
+      })
+
+      -- Keybindings for Trouble
+      vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, desc = "Toggle Trouble" })
+      vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { silent = true, desc = "Workspace Diagnostics" })
+      vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { silent = true, desc = "Document Diagnostics" })
+      vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, desc = "Quickfix List" })
+      vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, desc = "Location List" })
+    end
+  }
+end)
